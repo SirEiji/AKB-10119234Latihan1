7c4274d6a2a627ec3f47a452a368a7e24eda407e
SirEiji <sirarham@gmail.com>
2022-04-20 10:36:04 +0700

    first commit

---
+++ app/src/main/java/com/example/a10119234latihan1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a10119234latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+Tanggal Pengerjaan  : 18/04/2022
+NIM                 : 10119234
+Nama                : ARHAM JUSNI INDRAWAN
+Kelas               : IF - 6
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
